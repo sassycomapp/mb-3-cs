d24c3c2ecc70dbd88d69d8d1a057bed990a15603
sassycomapp@gmail.com <sassycomapp@gmail.com>
2026-06-22 12:44:25 +0000

    Created analytics.RevenueReportForm.RowTemplate7; Edited analytics.RevenueReportForm

---
+++ client_code/analytics/RevenueReportForm/RowTemplate7/__init__.py
@@ -0,0 +1,15 @@
+from ._anvil_designer import RowTemplate7Template
+from anvil import *
+import anvil.server
+from routing import router
+import m3.components as m3
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class RowTemplate7(RowTemplate7Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    super().__init__(**properties)
+
+    # Any code you write here will run before the form opens.
