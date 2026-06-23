77c31a05719276986cde1668d1f59f8473e6899e
sassycomapp@gmail.com <sassycomapp@gmail.com>
2026-06-23 15:08:06 +0000

    Created blog.BlogPostForm.CategoriesTemplate and blog.BlogPostForm.RelatedPostsTemplate; Edited blog.BlogPostForm; Deleted blog.BlogPostForm.ItemTemplate3

--- client_code/blog/BlogPostForm/CategoriesTemplate/__init__.py
+++ client_code/blog/BlogPostForm/CategoriesTemplate/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate3Template
+from ._anvil_designer import CategoriesTemplateTemplate
 from anvil import *
 import anvil.server
 from routing import router
@@ -7,7 +7,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class ItemTemplate3(ItemTemplate3Template):
+class CategoriesTemplate(CategoriesTemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
---
+++ client_code/blog/BlogPostForm/RelatedPostsTemplate/__init__.py
@@ -0,0 +1,15 @@
+from ._anvil_designer import RelatedPostsTemplateTemplate
+from anvil import *
+import anvil.server
+from routing import router
+import m3.components as m3
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class RelatedPostsTemplate(RelatedPostsTemplateTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    super().__init__(**properties)
+
+    # Any code you write here will run before the form opens.
