ea42aebc39de68092d8786707263a94cd1262c21
sassycomapp@gmail.com <sassycomapp@gmail.com>
2026-06-24 11:31:51 +0000

    Created blog.CategoryManagementForm.CategoryManagementTemplate; Edited blog.CategoryManagementForm; Deleted blog.CategoryManagementForm.RowTemplate2

--- client_code/blog/CategoryManagementForm/CategoryManagementTemplate/__init__.py
+++ client_code/blog/CategoryManagementForm/CategoryManagementTemplate/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate2Template
+from ._anvil_designer import CategoryManagementTemplateTemplate
 from anvil import *
 import anvil.server
 from routing import router
@@ -7,7 +7,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class RowTemplate2(RowTemplate2Template):
+class CategoryManagementTemplate(CategoryManagementTemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
